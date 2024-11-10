e51dc4d9214265f348b03667cabea52f4dd56ef5
Elavarasanb471210 <elavarasanbelavarasanb@gmail.com>
2024-11-10 11:09:40 +0530

    first commit

---
+++ Book_Store_Full_Stack-main/back-end/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongodbURL = "mongodb+srv://elavarasanbelavarasanb:DfvScjVgq7HQVlMm@cluster0.8jwdf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
